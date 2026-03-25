d3d11078cbbda3ea18eb450c9ec8c9e640269192
Ebert dos Santos Guerreiro <ebertg.est@gmail.com>
2026-03-24 23:13:48 -0300

    Iniciação com primeira tela e função no java para mostrar nome na tela com taost

--- app/src/main/java/com/example/apptest/MainActivity.java
+++ app/src/main/java/com/example/apptest/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.apptest;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +23,17 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        EditText campoUsusario = findViewById(R.id.editUsuario);
+        Button botaoEnter = findViewById(R.id.btnEntrar);
+
+        botaoEnter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nome = campoUsusario.getText().toString();
+                Toast.makeText(MainActivity.this, "Bem-Vindo" + nome, Toast.LENGTH_SHORT).show();
+            }
         });
     }
 }
