619f21446f9b4e90729f5b3a9d32602d288c759f
Ebert dos Santos Guerreiro <ebertg.est@gmail.com>
2026-04-11 18:39:02 -0300

    Ex12: Enviando dados para outra activity usando .putExtra()

--- app/src/main/java/com/example/apptest/MainActivity.java
+++ app/src/main/java/com/example/apptest/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,16 +24,17 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Button btnSendData = findViewById(R.id.btnSendData);
+        EditText etNameMain = findViewById(R.id.etNameMain);
 
-        Button btnGoToSecond = findViewById(R.id.btnGoToSecond);
-
-        btnGoToSecond.setOnClickListener(new View.OnClickListener() {
+        btnSendData.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String name = etNameMain.getText().toString();
                 Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra("USER_NAME", name);
                 startActivity(intent);
             }
         });
-
     }
 }
