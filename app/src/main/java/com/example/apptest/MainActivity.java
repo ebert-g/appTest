628eec53250933e51a2fdf3399418f84fcbdd6af
Ebert dos Santos Guerreiro <ebertg.est@gmail.com>
2026-04-11 18:03:44 -0300

    Main limpo, código nas branchs para cada lvl

--- app/src/main/java/com/example/apptest/MainActivity.java
+++ app/src/main/java/com/example/apptest/MainActivity.java
@@ -1,10 +1,6 @@
 package com.example.apptest;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -25,15 +21,5 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        EditText campoUsusario = findViewById(R.id.editUsuario);
-        Button botaoEnter = findViewById(R.id.btnEntrar);
-
-        botaoEnter.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String nome = campoUsusario.getText().toString();
-                Toast.makeText(MainActivity.this, "Bem-Vindo" + nome, Toast.LENGTH_SHORT).show();
-            }
-        });
     }
 }
