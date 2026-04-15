1f0d67dd9e10a5891f8f67b331b6ff1883629b2d
Ebert dos Santos Guerreiro <ebertg.est@gmail.com>
2026-04-15 00:34:06 -0300

    EX16 e EX  17

--- app/src/main/java/com/example/apptest/MainActivity.java
+++ app/src/main/java/com/example/apptest/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.apptest;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,6 +24,38 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        EditText etNameForm = findViewById(R.id.etNameForm);
+        EditText etEmailForm = findViewById(R.id.etEmailForm);
+        EditText etPasswordForm = findViewById(R.id.etPasswordForm);
+        Button btnLoginForm = findViewById(R.id.btnLogin);
+
+        btnLoginForm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = etNameForm.getText().toString().trim();
+                String email = etEmailForm.getText().toString().trim();
+                String password = etPasswordForm.getText().toString().trim();
+                if (name.isEmpty()) {
+                    etNameForm.setError("Name is Required");
+
+                } else if (email.isEmpty()) {
+                    etEmailForm.setError("Email is Required");
+
+                } else if (password.isEmpty()) {
+                    etPasswordForm.setError("Password is Required");
+
+                }
+
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra("NAME_FORM", name);
+                intent.putExtra("EMAIL_FORM", email);
+                startActivity(intent);
+
+
+            }
+        });
+
 
     }
 }
